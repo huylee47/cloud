8a414520fdc6a91ef1ca22ab6fcc711b6fa39e84
huylee47 <huyw300@gmail.com>
2025-05-03 12:40:24 +0700

    .

--- app/Models/CheckLog.php
+++ app/Models/CheckLog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Checklog extends Model
+class CheckLog extends Model
 {
     use HasFactory;
     protected $table = 'checklogs';
